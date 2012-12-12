00f72d7fb253c38bdee193d36dfd83a556142cb5
Jim Pruetting <jim@roboticmethod.com>
2012-12-12 13:48:22 -0600

    Remove front end routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Refinery::Core::Engine.routes.append do
 
-  # Frontend routes
-  namespace :image_slideshows do
-    resources :image_slideshows, :path => '', :only => [:index, :show]
-  end
-
   # Admin routes
   namespace :image_slideshows, :path => '' do
     namespace :admin, :path => 'refinery' do
@@ -14,12 +9,6 @@ Refinery::Core::Engine.routes.append do
         end
       end
     end
-  end
-
-
-  # Frontend routes
-  namespace :image_slides do
-    resources :image_slides, :path => '', :only => [:index, :show]
   end
 
   # Admin routes
