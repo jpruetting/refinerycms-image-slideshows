0d3953e87fc6a499b0467a69442a732307add9a1
Jim Pruetting <jim@roboticmethod.com>
2012-12-13 00:42:17 -0600

    Refactor image slides controller

--- app/controllers/refinery/image_slideshows/admin/image_slides_controller.rb
+++ app/controllers/refinery/image_slideshows/admin/image_slides_controller.rb
@@ -6,48 +6,53 @@ module Refinery
         crudify :'refinery/image_slideshows/image_slide', :sortable => true
 
         before_filter :find_image_slideshow
+        before_filter :find_image_slide, :except => [:index, :new]
 
         def index
           find_image_slides
         end
 
         def create
-          params[:image_slide].merge!({
-            :position => Refinery::ImageSlideshows::ImageSlide.maximum(:position) + 1
-          })
-
-          @image_slide = Refinery::ImageSlideshows::ImageSlide.new(params[:image_slide])
+          @image_slide.position = Refinery::ImageSlideshows::ImageSlide.maximum(:position) + 1
 
           if @image_slide.valid? && @image_slide.save
-            redirect_to(refinery.image_slideshows_admin_image_slideshow_image_slides_path(@image_slideshow), :notice => 'Image slide was successfully created.')
+            redirect_to image_slides_path, :notice => 'Image slide was successfully created.'
+          else
+            render :action => :new
           end
         end
 
         def update
-          @image_slide = Refinery::ImageSlideshows::ImageSlide.find(params[:id])
- 
           if @image_slide.update_attributes(params[:image_slide])
-            redirect_to(refinery.image_slideshows_admin_image_slideshow_image_slides_path(@image_slideshow), :notice => 'Image slide was successfully updated.') 
+            redirect_to image_slides_path, :notice => 'Image slide was successfully updated.'
+          else
+            render :action => :edit
           end
         end
 
         def destroy
-          @image_slide = Refinery::ImageSlideshows::ImageSlide.find(params[:id])
-
           if @image_slide.destroy
-            flash[:notice] = t('crudify.deleted', :what => @image_slide.class.to_s.underscore.humanize)
-            redirect_to refinery.image_slideshows_admin_image_slideshow_image_slides_path(@image_slideshow)
+            redirect_to image_slides_path, :notice => 'Image slide was successfully deleted.'
           end
         end
 
         private
+
+        def image_slides_path
+          refinery.image_slideshows_admin_image_slideshow_image_slides_path(@image_slideshow)
+        end
+
+        def find_image_slide
+          @image_slide = Refinery::ImageSlideshows::ImageSlide.find(params[:id]) if params[:id]
+          @image_slide ||= Refinery::ImageSlideshows::ImageSlide.new(params[:image_slide]) if params[:image_slide]
+        end
 
         def find_image_slideshow
           @image_slideshow = Refinery::ImageSlideshows::ImageSlideshow.find(params[:image_slideshow_id])
         end
 
         def find_image_slides
-          @image_slides = @image_slideshow.present? ? @image_slideshow.image_slides.order(:position) : Refinery::ImageSlideshows::ImageSlide.all
+          @image_slides = @image_slideshow.image_slides.order(:position) if @image_slideshow.present?
         end
 
       end
