ffa72d2f12258a7724a909fc6409014d213dc9c2
Jim Pruetting <jim@roboticmethod.com>
2012-12-12 23:45:24 -0600

    Make image slide routes nested.

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,12 @@ Refinery::Core::Engine.routes.append do
   # Admin routes
   namespace :image_slideshows, :path => '' do
     namespace :admin, :path => 'refinery' do
-      resources :image_slideshows, :except => :show do
-        collection do
-          post :update_positions
+      resources :image_slideshows do
+        resources :image_slides, :except => :show do
+          collection do
+            post :update_positions
+          end
         end
-      end
-    end
-  end
-
-  # Admin routes
-  namespace :image_slides, :path => '' do
-    namespace :admin, :path => 'refinery' do
-      resources :image_slides, :except => :show do
         collection do
           post :update_positions
         end
