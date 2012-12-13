bccc60d11f79d16617da59bc7fe2761819cf933b
Jim Pruetting <jim@roboticmethod.com>
2012-12-13 00:42:37 -0600

    Clean up image slide model

--- app/models/refinery/image_slideshows/image_slide.rb
+++ app/models/refinery/image_slideshows/image_slide.rb
@@ -3,14 +3,14 @@ module Refinery
     class ImageSlide < Refinery::Core::BaseModel
       self.table_name = 'refinery_image_slides'
 
-      attr_accessible :title, :position, :image_slideshow_id, :caption, :image_id
+      attr_accessible :title, :image_id, :caption, :position, :image_slideshow_id
 
       acts_as_indexed :fields => [:title]
 
       validates :title, :presence => true
+      validates :image_id, :presence => true
 
       belongs_to :image_slideshow
-
       belongs_to :image
     end
   end
