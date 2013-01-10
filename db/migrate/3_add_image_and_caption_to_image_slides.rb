1c61f8f2079e8d61cf6cda5fe3b09c5e7e1a9e04
Jim Pruetting <jim@roboticmethod.com>
2013-01-10 15:32:37 -0600

    Fix migration column type for image.

--- db/migrate/3_add_image_and_caption_to_image_slides.rb
+++ db/migrate/3_add_image_and_caption_to_image_slides.rb
@@ -1,6 +1,6 @@
 class AddImageAndCaptionToImageSlides < ActiveRecord::Migration
   def change
-    add_column :refinery_image_slides, :image, :image
+    add_column :refinery_image_slides, :image_id, :integer
     add_column :refinery_image_slides, :caption, :string
   end
 end
